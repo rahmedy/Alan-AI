49ff80f5fc72dcc533cefe7f21a54bed12e79aca
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-12-09 02:37:02 -0500

    added open cart abiltiy with custom event

--- alan-1/src/hooks/useAlan.js
+++ alan-1/src/hooks/useAlan.js
@@ -1,21 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { useCart } from '../context/CartContext';
+
+const COMMANDS = {
+	OPEN_CART: 'open-cart'
+};
 
 export default function useAlan() {
+	const [ alanInstance, setAlanInstance ] = useState();
+	const { setShowCartItems } = useCart();
 
+	const openCart = useCallback(
+		() => {
+			alanInstance.playText('Opening cart')
+			setShowCartItems(true)
+		},	[ alanInstance ]);
+
+	useEffect(
+		() => {
+			window.addEventListener(COMMANDS.OPEN_CART, openCart);
+			return () => {
+				window.removeEventListener(COMMANDS.OPEN_CART, openCart);
+			};
+		},
+		[ openCart ]
+	);
 
 	useEffect(() => {
+		if (alanInstance != null) return;
 
-        alanBtn({
-            key: process.env.REACT_APP_ALAN_KEY,
-            onCommand: (commandData) => {
-                console.log(commandData);
-                
-            }
-        })
-
-
-    }, []);
+		setAlanInstance(
+			alanBtn({
+				key: process.env.REACT_APP_ALAN_KEY,
+				onCommand: ({ command }) => {
+					window.dispatchEvent(new CustomEvent(command));
+				}
+			})
+		);
+	}, []);
 
 	return null;
 }
