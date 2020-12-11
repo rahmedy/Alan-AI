62176a2674803eb064809be44a203e74d9be0894
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-12-10 21:53:43 -0500

    createrd fucntion for adding itmes to the cart with alan

--- alan-1/src/hooks/useAlan.js
+++ alan-1/src/hooks/useAlan.js
@@ -1,48 +1,65 @@
 import { useEffect, useState, useCallback } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import { useCart } from '../context/CartContext';
+import storeItems from '../items.json'
 
 const COMMANDS = {
 	OPEN_CART: 'open-cart',
-	CLOSE_CART:'close-cart'
+	CLOSE_CART: 'close-cart',
+	ADD_ITEM: 'add-item'
 };
 
 export default function useAlan() {
 	const [ alanInstance, setAlanInstance ] = useState();
-	const { setShowCartItems, isCartEmpty } = useCart();
+	const { setShowCartItems, isCartEmpty, addToCart } = useCart();
 
-	const openCart = useCallback(() => {
-			if(isCartEmpty){
-				alanInstance.playText('You have no items in your cart')
+	const openCart = useCallback(
+		() => {
+			if (isCartEmpty) {
+				alanInstance.playText('You have no items in your cart');
 			} else {
-				alanInstance.playText("Opening cart")
-				setShowCartItems(true)
+				alanInstance.playText('Opening cart');
+				setShowCartItems(true);
 			}
-
 		},
-		[ alanInstance,isCartEmpty, setShowCartItems]
+		[ alanInstance, isCartEmpty, setShowCartItems ]
 	);
 
-	const closeCart = useCallback(() => {
-		if(isCartEmpty){
-			alanInstance.playText('You have no items in your cart')
+	const closeCart = useCallback(
+		() => {
+			if (isCartEmpty) {
+				alanInstance.playText('You have no items in your cart');
+			} else {
+				alanInstance.playText('Closing cart');
+				setShowCartItems(false);
+			}
+		},
+		[ alanInstance, isCartEmpty, setShowCartItems ]
+	);
+
+	const addItem = useCallback(({detail: {name, quantity}}) =>{
+		const item = storeItems.find(i => i.name.toLowerCase() === name.toLowerCase())
+		if(item == null) {
+			alanInstance.playText(`I cannot find the ${name} item `)
 		} else {
-			alanInstance.playText("Closing cart")
-			setShowCartItems(false)
+			addToCart(item.id, quantity)
+			alanInstance.playText(`Added ${quantity} of the ${name} item to your cart`)
 		}
 
-	},
-	[ alanInstance,isCartEmpty, setShowCartItems]
-);
+
+	})
 
 	useEffect(
 		() => {
 			window.addEventListener(COMMANDS.OPEN_CART, openCart);
 			window.addEventListener(COMMANDS.CLOSE_CART, closeCart);
+			window.addEventListener(COMMANDS.ADD_ITEM, addItem);
+
 
 			return () => {
 				window.removeEventListener(COMMANDS.OPEN_CART, openCart);
 				window.removeEventListener(COMMANDS.CLOSE_CART, closeCart);
+				window.removeEventListener(COMMANDS.ADD_ITEM, addItem);
 
 			};
 		},
@@ -55,8 +72,8 @@ export default function useAlan() {
 		setAlanInstance(
 			alanBtn({
 				key: process.env.REACT_APP_ALAN_KEY,
-				onCommand: ({ command }) => {
-					window.dispatchEvent(new CustomEvent(command));
+				onCommand: ({ command, payload }) => {
+					window.dispatchEvent(new CustomEvent(command, {detail: payload}));
 				}
 			})
 		);
